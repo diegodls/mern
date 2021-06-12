a22f71a0d2580ed188737b7ff4716451129323a5
Diego Luis <contato.diegoluisdasilva@gmail.com>
2021-06-12 18:15:54 -0300

    Adicionando Maps, UserList e Mais

---
+++ frontend_mern/src/shared/components/UIElements/Map.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./Map.css";
+
+const Map = (props) => {
+  const { lat, lng } = props.coordinates;
+  const zoom = props.zoom;
+  const position = [51.505, -0.09];
+
+  return (
+   <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  );
+};
+
+export default Map;
